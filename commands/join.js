091e8a9e38e21759f8a76476dd1afabe1f87977d
stranothus <stranothus@gmail.com>
2021-12-28 15:25:17 -0600

    Join and leave voice chats

---
+++ commands/join.js
@@ -0,0 +1,41 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { joinVoiceChannel } from '@discordjs/voice';
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("join")
+        .setDescription("Joins your voice chat"),
+    DMs: false,
+    execute: function(interaction) {
+        const channel = interaction.member.voice.channel;
+
+        if(!channel) {
+            interaction.reply("Please connect to a voice channel first");
+            return;
+        }
+
+        joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guild.id,
+            adapterCreator: channel.guild.voiceAdapterCreator
+        });
+
+        interaction.reply(`Joined <#${channel.id}>`);
+    },
+    executeText: async function(msg, args) {
+        const channel = (await msg.guild.members.fetch(msg.author.id)).voice.channel;
+
+        if(!channel) {
+            msg.channel.send("Please connect to a voice channel first");
+            return;
+        }
+
+        joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guild.id,
+            adapterCreator: channel.guild.voiceAdapterCreator
+        });
+
+        msg.channel.send(`Joined <#${channel.id}>`);
+    }
+}
---
+++ commands/leave.js
@@ -0,0 +1,33 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { getVoiceConnection } from '@discordjs/voice';
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("leave")
+        .setDescription("Leaves your voice chat"),
+    DMs: false,
+    execute: function(interaction) {
+        const connection = getVoiceConnection(interaction.guild.id);
+
+        if(!connection) {
+            interaction.reply("Not connected to any voice chat");
+            return;
+        }
+
+        connection.destroy();
+
+        interaction.reply(`Left <#${connection.joinConfig.channelId}>`);
+    },
+    executeText: function(msg, args) {
+        const connection = getVoiceConnection(msg.guild.id);
+
+        if(!connection) {
+            msg.channel.send("Not connected to any voice chat");
+            return;
+        }
+
+        connection.destroy();
+
+        msg.channel.send(`Left <#${connection.joinConfig.channelId}>`);
+    }
+}
