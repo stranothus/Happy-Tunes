ff38b02b019141921354bf5a45bdf608b743a7ff
stranothus <stranothus@gmail.com>
2021-12-29 08:38:48 -0600

    Pause and resume songs

---
+++ commands/pause.js
@@ -0,0 +1,33 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { getVoiceConnection } from '@discordjs/voice';
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("pause")
+        .setDescription("Pause the current song"),
+    DMs: false,
+    execute: async function(interaction) {
+        const connection = getVoiceConnection(interaction.guild.id);
+
+        if(!connection) {
+            interaction.reply("Not connected to any voice chat");
+            return;
+        }
+
+        connection._state.subscription.player.pause();
+
+        interaction.reply("The song has been paused");
+    },
+    executeText: async function(msg, args) {
+        const connection = getVoiceConnection(msg.guild.id);
+
+        if(!connection) {
+            msg.channel.send("Not connected to any voice chat");
+            return;
+        }
+
+        connection._state.subscription.player.pause();
+
+        msg.channel.send("The song has been paused");
+    }
+}
---
+++ commands/resume.js
@@ -0,0 +1,33 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { getVoiceConnection } from '@discordjs/voice';
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("resume")
+        .setDescription("Unpause the current song"),
+    DMs: false,
+    execute: async function(interaction) {
+        const connection = getVoiceConnection(interaction.guild.id);
+
+        if(!connection) {
+            interaction.reply("Not connected to any voice chat");
+            return;
+        }
+
+        connection._state.subscription.player.unpause();
+
+        interaction.reply("The song has been resumed");
+    },
+    executeText: async function(msg, args) {
+        const connection = getVoiceConnection(msg.guild.id);
+
+        if(!connection) {
+            msg.channel.send("Not connected to any voice chat");
+            return;
+        }
+
+        connection._state.subscription.player.unpause();
+
+        msg.channel.send("The song has been resumed");
+    }
+}
