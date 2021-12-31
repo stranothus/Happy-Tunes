629a489e0d418b03f5b1cb7671bb958e52e18aa6
stranothus-studios <>
2021-12-31 17:53:20 +0000

    Add search command Search for a song and get the top ten results. Use the select menu to select one to play. The message is then edited to remove the select menu and embeds describing each result.

---
+++ commands/search.js
@@ -0,0 +1,147 @@
+import { MessageEmbed, MessageActionRow, MessageSelectMenu } from "discord.js";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { createAudioPlayer, createAudioResource, getVoiceConnection, AudioPlayerStatus } from '@discordjs/voice';
+import Innertube from "youtubei.js";
+import ytdl from "ytdl-core";
+
+const youtube = await new Innertube();
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("search")
+        .setDescription("Search for music to play")
+        .addStringOption(option => option
+            .setName("search")
+            .setDescription("The music to search for")
+            .setRequired(true)
+        ),
+    DMs: false,
+    execute: async function(interaction) {
+    	const connection = getVoiceConnection(interaction.guild.id);
+
+        if(!connection) {
+            interaction.reply("Not connected to any voice chat");
+            return;
+        }
+
+        const searchFor = interaction.options.getString("search");
+
+        if(!searchFor) {
+            interaction.reply("No search criteria provided");
+            return;
+        }
+
+        interaction.deferReply();
+
+        const results = await youtube.search(searchFor);
+
+        if(!results.videos.length) {
+            interaction.editReply("No videos found");
+            return;
+        }
+
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageSelectMenu()
+					.setCustomId('select')
+					.setPlaceholder('Nothing selected')
+					.addOptions(results.videos.slice(0, 10).map(v => ({
+						label: v.title,
+						description: v.description.substring(0, 100),
+						value: v.url.split("/").reverse()[0]
+					}))),
+			);
+
+        interaction.editReply({
+			content: `**Search results for** *${searchFor}*`,
+			embeds: results.videos.slice(0, 10).map(v => 
+				new MessageEmbed()
+					.setTitle(v.title)
+					.setURL(v.url)
+					.setDescription(v.description.substring(0, 100))
+					.setAuthor(v.author)
+					.setThumbnail(v.metadata.thumbnails[0].url)
+			),
+			components: [row]
+		});
+    },
+    executeText: async function(msg, args) {
+        const connection = getVoiceConnection(msg.guild.id);
+
+        if(!connection) {
+            msg.channel.send("Not connected to any voice chat");
+            return;
+        }
+
+        const searchFor = interaction.options.getString("search");
+
+        if(!searchFor) {
+            msg.channel.send("No search criteria provided");
+            return;
+        }
+
+        const results = await youtube.search(searchFor);
+
+        if(!results.videos.length) {
+            msg.channel.send("No videos found");
+            return;
+        }
+
+        const row = new MessageActionRow()
+			.addComponents(
+				new MessageSelectMenu()
+					.setCustomId('select')
+					.setPlaceholder('Nothing selected')
+					.addOptions(results.videos.slice(0, 10).map(v => ({
+						label: v.title,
+						description: v.description.substring(0, 100),
+						value: v.url.split("/").reverse()[0]
+					}))),
+			);
+
+        msg.channel.send({
+			content: `**Search results for** *${searchFor}*`,
+			embeds: results.videos.slice(0, 10).map(v => 
+				new MessageEmbed()
+					.setTitle(v.title)
+					.setURL(v.url)
+					.setDescription(v.description.substring(0, 100))
+					.setAuthor(v.author)
+					.setThumbnail(v.metadata.thumbnails[0].url)
+			),
+			components: [row]
+		});
+    },
+	selectMenu: async function(interaction) {
+		const connnection = getVoiceConnection(interaction.guild.id);
+
+		if(!connnection) return;
+
+		interaction.update({ content: 'A song was selected!', components: [], embeds: [] });
+
+		const key = interaction.values[0];
+
+		const video = ytdl("https://www.youtube.com/watch?v=" + key, {
+            filter: "audioonly",
+        });
+        const player = createAudioPlayer();
+        const resource = createAudioResource(video);
+
+        connnection.subscribe(player);
+        player.play(resource);
+
+        player.on('error', console.error);
+        player.on(AudioPlayerStatus.Playing, () => {
+            console.log('The audio player has started playing!');
+        });
+        player.on(AudioPlayerStatus.Paused, () => {
+            console.log('The audio player has been paused');
+        });
+        player.on(AudioPlayerStatus.AutoPaused, () => {
+            console.log('The audio player has been auto-paused');
+        });
+        player.on(AudioPlayerStatus.Idle, () => {
+            console.log("Idle");
+        });
+	}
+}
