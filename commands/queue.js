ecb46001a4cf3674152256767a4e9d4ae04316da
stranothus-studios <>
2022-01-04 19:29:55 +0000

    Add queue viewer

---
+++ commands/queue.js
@@ -0,0 +1,63 @@
+import { MessageEmbed } from "discord.js";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { getVoiceConnection } from '@discordjs/voice';
+import InnerTube from "youtubei.js";
+
+const youtube = await new InnerTube();
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName("queue")
+		.setDescription("View the current queue"),
+	DMs: false,
+	execute: async function(interaction) {
+		const connection = getVoiceConnection(interaction.guild.id);
+
+		if(!connection) {
+			interaction.reply("Not connected to any voice chat");
+			return;
+		}
+
+		const songs = interaction.client.servers[interaction.guild.id];
+
+		if(!songs.length) {
+			interaction.reply("No songs currently playing or in queue");
+			return;
+		}
+		
+		const embed = new MessageEmbed()
+			.setTitle(`The Queue`)
+			.setDescription((await Promise.all(songs.map(async (id, i) => {
+				const details = await youtube.getDetails(id);
+				
+				return `${i || "Currently playing.."}. [${details.title}](<https://www.youtube.com/watch?v=${id}>)`;
+			}))).join("\n"));
+
+		interaction.reply({ embeds: [embed] });
+	},
+	executeText: async function(msg, args) {
+		const connection = getVoiceConnection(msg.guild.id);
+
+		if(!connection) {
+			msg.channel.send("Not connected to any voice chat");
+			return;
+		}
+
+		const songs = msg.client.servers[msg.guild.id];
+
+		if(!songs.length) {
+			msg.channel.send("No songs currently playing or in queue");
+			return;
+		}
+		
+		const embed = new MessageEmbed()
+			.setTitle(`The Queue`)
+			.setDescription((await Promise.all(songs.map(async (id, i) => {
+				const details = await youtube.getDetails(id);
+				
+				return `${i || "Currently playing.."}. [${details.title}](<https://www.youtube.com/watch?v=${id}>)`;
+			}))).join("\n"));
+
+		msg.channel.send({ embeds: [embed] });
+	}
+}
--- commands/skip.js
+++ commands/skip.js
@@ -8,7 +8,12 @@ const youtube = await new Innertube();
 export default {
 	data: new SlashCommandBuilder()
 		.setName("skip")
-		.setDescription("Skip the current song"),
+		.setDescription("Skip the current song")
+		.addStringOption(option => option
+			.setName("skipto")
+			.setDescription("the song to skip to in the queue")
+			.setRequired(false)
+		),
 	DMs: false,
 	execute: async function(interaction) {
 		const connection = getVoiceConnection(interaction.guild.id);
@@ -18,19 +23,31 @@ export default {
 			return;
 		}
 
-		const player = connection._state.subscription.player;
 		const songs = interaction.client.servers[interaction.guild.id];
+		const player = connection._state.subscription.player;
+		const skipTo = interaction.options.getString("skipto") || 1;
 
 		if(!player || !songs || !songs.length) {
 			interaction.reply("There are no songs playing");
 			return;
 		}
 
-		interaction.reply(`Skipped https://www.youtube.com/watch?v=${songs[0]}. Playing https://www.youtube.com/watch?v=${songs[1]}`);
+		const oldSong = songs[0];
+		const newSong = skipTo >= songs.length ? undefined : songs[skipTo];
 
-		interaction.client.servers[interaction.guild.id].shift();
+		for(let i = skipTo; i < skipTo && interaction.client.servers[interaction.guild.id].length; i++) {
+			interaction.client.servers[interaction.guild.id].shift();
+		}
 
-		const video = ytdl("https://www.youtube.com/watch?v=" + songs[0], {
+		if(!newSong) {
+			interaction.reply(`Skipped https://www.youtube.com/watch?v=${oldSong}. No more songs to play`);
+			player.stop();
+			return;
+		}
+
+		interaction.reply(`Skipped https://www.youtube.com/watch?v=${oldSong}. Playing https://www.youtube.com/watch?v=${newSong}`);
+
+		const video = ytdl("https://www.youtube.com/watch?v=" + newSong, {
 			filter: "audioonly",
 		});
 		const resource = createAudioResource(video);
@@ -45,19 +62,31 @@ export default {
 			return;
 		}
 
-		const player = connection._state.subscription.player;
 		const songs = msg.client.servers[msg.guild.id];
+		const player = connection._state.subscription.player;
+		const skipTo = args[0] || 1;
 
 		if(!player || !songs || !songs.length) {
-			msg.channel.send("There are no songs playing");
+			interaction.reply("There are no songs playing");
 			return;
 		}
 
-		msg.channel.send(`Skipped https://www.youtube.com/watch?v=${songs[0]}. Playing https://www.youtube.com/watch?v=${songs[1]}`);
+		const oldSong = songs[0];
+		const newSong = skipTo >= songs.length ? undefined : songs[skipTo];
 
-		msg.client.servers[msg.guild.id].shift();
+		for(let i = skipTo; i < skipTo && msg.client.servers[msg.guild.id].length; i++) {
+			msg.client.servers[msg.guild.id].shift();
+		}
 
-		const video = ytdl("https://www.youtube.com/watch?v=" + songs[0], {
+		if(!newSong) {
+			msg.channel.send(`Skipped https://www.youtube.com/watch?v=${oldSong}. No more songs to play`);
+			player.stop();
+			return;
+		}
+
+		msg.channel.send(`Skipped https://www.youtube.com/watch?v=${oldSong}. Playing https://www.youtube.com/watch?v=${newSong}`);
+
+		const video = ytdl("https://www.youtube.com/watch?v=" + newSong, {
 			filter: "audioonly",
 		});
 		const resource = createAudioResource(video);
